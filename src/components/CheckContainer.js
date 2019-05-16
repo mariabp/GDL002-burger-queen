1cf08852ce8cd2d70ea98dc9977ef47f79d1b16a
Maria Bolaños <mariadlp@gmail.com>
2019-05-16 12:55:20 -0500

    Component for displaying the check container

---
+++ src/components/CheckContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class CheckContainer extends React.Component {
+    render() {
+		return <div className="check-container">
+					<div className="tableId">MESA 1</div>
+					<div className="tableOrder"></div>
+					<button className="sendOrder">ENVIAR COMANDA</button>
+			</div>
+    }
+  }
