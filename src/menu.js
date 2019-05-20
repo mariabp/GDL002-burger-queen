3d5ba0f783b75b4162d151f9c19c12b0fe6ebb1a
Maria Bolaños <mariadlp@gmail.com>
2019-05-20 11:39:30 -0500

    I added new items to de menu

--- src/menu.js
+++ src/menu.js
@@ -8,6 +8,14 @@ const MENU =  [
 
 	},
 	{
+		id: 102,
+		name: 'French Toast',
+		price: 35,
+		type: 'breakfast'
+
+	},
+
+	{
 		id: 201,
 		name: 'Hamburguesa Sencilla',
 		price: 35,
@@ -15,8 +23,22 @@ const MENU =  [
 
 	},
 	{
+		id: 203,
+		name: 'Hamburguesa Doble',
+		price: 50,
+		type: 'meals'
+
+	},
+	{
 		id: 301,
 		name: 'Kombucha CH',
+		price: 35,
+		type: 'beverages'
+
+	},
+	{
+		id: 302,
+		name: 'Agua CH',
 		price: 35,
 		type: 'beverages'
 
