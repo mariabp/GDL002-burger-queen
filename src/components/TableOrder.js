e3c7abf2b7c0ba3ea212e1188051710158eea773
Maria Bolaños <mariadlp@gmail.com>
2019-05-21 07:56:08 -0500

    The component for the table order was created

--- src/components/TableOrder.js
+++ src/components/TableOrder.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import OrderItem from './OrderItem';
+import OrderedItem from './OrderedItem';
 
 class TableOrder extends React.Component {
 
 	render() {
 
-		return <div className="table-order">
-				  <OrderItem />
-				</div>
+		let n = 0;
+
+		let mapOrder = this.props.order.map((item) => {
+
+			n++;
+
+			return <OrderedItem name={item.name} price={item.price} key={n} id={n} type={item.type} removeItem={this.props.removeItem} />
+
+		});
+
+		return (
+
+			<div className="table-order">
+
+				  {mapOrder}
+
+			</div>
+
+		)
 
 	}
 
