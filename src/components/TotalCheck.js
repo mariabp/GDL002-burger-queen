6cb07843a55dbb745813cd8f3abda9cde73d99e4
Maria Bolaños <mariadlp@gmail.com>
2019-05-22 19:55:43 -0500

    The component for displaying the total amount to pay, is done.

---
+++ src/components/TotalCheck.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class TotalCheck extends React.Component {
+
+	render() {
+
+		return <div className="total-check">TOTAL</div>
+
+	}
+
+};
+
+export default TotalCheck;
