18682f65376293e33e3ecf57671cab7ef032a9f9
Maria Bolaños <mariadlp@gmail.com>
2019-05-23 10:52:13 -0500

    The full menu has been updated

--- src/menu.js
+++ src/menu.js
@@ -3,7 +3,7 @@ const MENU =  [
 	{
 		id: 101,
 		name: 'Hot Cakes',
-		price: 35,
+		price: 30,
 		type: 'breakfast'
 
 	},
@@ -12,36 +12,79 @@ const MENU =  [
 		name: 'French Toast',
 		price: 35,
 		type: 'breakfast'
+	},
+	{
+		id: 104,
+		name: 'Ensalada de Frutas',
+		price: 25,
+		type: 'breakfast'
 
 	},
-
 	{
 		id: 201,
 		name: 'Hamburguesa Sencilla',
 		price: 35,
 		type: 'meals'
-
 	},
 	{
 		id: 203,
 		name: 'Hamburguesa Doble',
-		price: 50,
+		price: 45,
 		type: 'meals'
-
+	},
+	{
+		id: 204,
+		name: 'Papas Gajo',
+		price: 25,
+		type: 'meals'
+	},
+	{
+		id: 205,
+		name: 'Ensalada De La Casa',
+		price: 25,
+		type: 'meals'
 	},
 	{
 		id: 301,
-		name: 'Kombucha CH',
+		name: 'Café Americano',
 		price: 35,
 		type: 'beverages'
-
 	},
 	{
 		id: 302,
-		name: 'Agua CH',
-		price: 35,
+		name: 'Café Au Lait',
+		price: 25,
 		type: 'beverages'
-
+	},
+	{
+		id: 303,
+		name: 'Té',
+		price: 20,
+		type: 'beverages'
+	},
+	{
+		id: 304,
+		name: 'Kombucha CH',
+		price: 25,
+		type: 'beverages'
+	},
+	{
+		id: 305,
+		name: 'Kombucha GDE',
+		price: 30,
+		type: 'beverages'
+	},
+	{
+		id: 306,
+		name: 'Agua CH',
+		price: 20,
+		type: 'beverages'
+	},
+	{
+		id: 307,
+		name: 'Agua GDE',
+		price: 25,
+		type: 'beverages'
 	},
 
 ];
