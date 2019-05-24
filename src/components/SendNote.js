bf51a62790641e30f9740455a0ea24486fa7929b
Maria Bolaños <mariadlp@gmail.com>
2019-05-23 20:26:01 -0500

    SendNote button with no functionality

---
+++ src/components/SendNote.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SendNote.css';
+
+
+class SendNote extends React.Component {
+
+	render() {
+
+		return (
+
+			<button className="send-note">ENVIAR NOTA</button>
+		)
+
+	}
+
+}
+
+export default SendNote;
