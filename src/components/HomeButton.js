df8b62ca31da2f20d6ab6cb7d8b77641bf1f9585
Maria Bolaños <mariadlp@gmail.com>
2019-05-23 20:25:35 -0500

    Home button without functionality

---
+++ src/components/HomeButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HomeButton.css';
+
+
+class HomeButton extends React.Component {
+
+	render() {
+
+		return (
+
+			<button className="home-button">INICIO</button>
+		)
+
+	}
+
+}
+
+export default HomeButton;
