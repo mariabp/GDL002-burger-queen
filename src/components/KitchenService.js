18159b4caa78f76c1dfc52e1643ef7e53590a60c
Maria Bolaños <mariadlp@gmail.com>
2019-06-27 15:57:25 -0500

    Added Kitchen Screen to prepare interface for next step

--- src/components/KitchenService.js
+++ src/components/KitchenService.js
@@ -3,7 +3,16 @@ import React from 'react';
 const KitchenService = props => (
 
 	<div className="kitchen-service-container">
-		<h5>kitchen service in construction</h5>
+		<div className="kitchen-display">
+			<div className="display-waiter">MESERO</div>
+			<div className="display-date">HORA</div>
+			<div className="display-table">MESA</div>
+			<div className="display-order">ORDEN</div>
+			<div className="display-preparing">COCINANDO</div>
+			<div className="display-deliver">LISTO</div>
+		</div>
+		<div className="display-orders"></div>
+
 	</div>
 )
 
